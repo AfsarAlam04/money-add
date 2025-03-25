68b47675fb95f234e01f3e423d0ba159dc9d1588
AfsarAlam04 <146763674+AfsarAlam04@users.noreply.github.com>
2025-03-26 02:11:15 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-const API_BASE = "https://rectangular-ballistic-tractor.glitch.me";
-
-// Example: Fetch apps from backend
-async function loadApps() {
-  const response = await fetch(`${API_BASE}/apps`);
-  const apps = await response.json();
-  // Populate dropdowns
-}
